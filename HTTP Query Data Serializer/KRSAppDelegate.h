20d3818c5fdb962dc27ac2a95002a2571c71f02e
Tom Krush <tom@tomkrush.com>
2014-01-16 13:02:26 -0600

    Initial Commit

---
+++ HTTP Query Data Serializer/KRSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KRSAppDelegate.h
+//  HTTP Query Data Serializer
+//
+//  Created by Tom Krush on 1/16/14.
+//  Copyright (c) 2014 Tom Krush. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KRSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HTTP Query Data Serializer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HTTP Query Data Serializer
+//
+//  Created by Tom Krush on 1/16/14.
+//  Copyright (c) 2014 Tom Krush. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "KRSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([KRSAppDelegate class]));
+    }
+}
