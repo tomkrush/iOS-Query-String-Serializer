f0ebbadda98c9cbe12cfbbf39879ffb156a2cb99
Tom Krush <tom@tomkrush.com>
2014-01-16 18:34:33 -0600

    Updated coding style to LLVM

--- HTTP Query Data Serializer/KRSQueryDataSerializer.h
+++ HTTP Query Data Serializer/KRSQueryDataSerializer.h
@@ -8,22 +8,24 @@
 
 #import <Foundation/Foundation.h>
 
-extern NSString * KRSHTTPBuildKeyPath(NSString *keyPath, NSString *key);
+extern NSString *KRSHTTPBuildKeyPath(NSString *keyPath, NSString *key);
 
-extern NSString * KRSHTTPKeyEncode(NSString *key);
+extern NSString *KRSHTTPKeyEncode(NSString *key);
 
-extern NSString * KRSHTTPValueEncode(NSString *value);
+extern NSString *KRSHTTPValueEncode(NSString *value);
 
-extern NSString * KRSHTTPValue(NSString *keyPath, id value);
+extern NSString *KRSHTTPValue(NSString *keyPath, id value);
 
 @interface KRSQueryDataSerializer : NSObject
 
 + (NSString *)stringFromDictionary:(NSDictionary *)dictionary;
 
-+ (NSString *)stringFromDictionary:(NSDictionary *)dictionary withKeyPath:(NSString *)keyPath;
++ (NSString *)stringFromDictionary:(NSDictionary *)dictionary
+                       withKeyPath:(NSString *)keyPath;
 
 + (NSString *)stringFromArray:(NSArray *)array withKeyPath:(NSString *)keyPath;
 
-+ (NSString *)stringFromUnescapedString:(NSString *)string withKeyPath:(NSString *)keyPath;
++ (NSString *)stringFromUnescapedString:(NSString *)string
+                            withKeyPath:(NSString *)keyPath;
 
 @end
--- HTTP Query Data Serializer/KRSQueryDataSerializer.m
+++ HTTP Query Data Serializer/KRSQueryDataSerializer.m
@@ -8,119 +8,105 @@
 
 #import "KRSQueryDataSerializer.h"
 
-NSString * KRSHTTPBuildKeyPath(NSString *keyPath, NSString *key) {
-    key = KRSHTTPKeyEncode(key);
+NSString *KRSHTTPBuildKeyPath(NSString *keyPath, NSString *key) {
+  key = KRSHTTPKeyEncode(key);
 
-    if ( ! keyPath )
-    {
-        return key;
-    }
+  if (!keyPath) {
+    return key;
+  }
 
-    keyPath = [NSString stringWithFormat:@"%@[%@]", keyPath, key];
-    
-    return keyPath;
+  keyPath = [NSString stringWithFormat:@"%@[%@]", keyPath, key];
+
+  return keyPath;
 }
 
-NSString * KRSHTTPKeyEncode(NSString *key) {
-    // Encode all the reserved characters, per RFC 3986
-    // (<http://www.ietf.org/rfc/rfc3986.txt>)
-    NSString *escaped = (NSString *)
-    CFBridgingRelease(CFURLCreateStringByAddingPercentEscapes(kCFAllocatorDefault,
-                                                              (CFStringRef)key,
-                                                              NULL,
-                                                              (CFStringRef)@"!*'();:@&=+$,/?%#",
-                                                            kCFStringEncodingUTF8));
-    return escaped;
+NSString *KRSHTTPKeyEncode(NSString *key) {
+  // Encode all the reserved characters, per RFC 3986
+  // (<http://www.ietf.org/rfc/rfc3986.txt>)
+  NSString *escaped =
+      (NSString *)CFBridgingRelease(CFURLCreateStringByAddingPercentEscapes(
+          kCFAllocatorDefault, (CFStringRef)key, NULL,
+          (CFStringRef) @"!*'();:@&=+$,/?%#", kCFStringEncodingUTF8));
+  return escaped;
 }
 
-NSString * KRSHTTPValueEncode(NSString *value) {
-    // Encode all the reserved characters, per RFC 3986
-    // (<http://www.ietf.org/rfc/rfc3986.txt>)
-    NSString *escaped = (NSString *)
-    CFBridgingRelease(CFURLCreateStringByAddingPercentEscapes(kCFAllocatorDefault,
-                                                              (CFStringRef)value,
-                                                              NULL,
-                                                              (CFStringRef)@"!*'();:@&=+$,/?%#[]",
-                                                            kCFStringEncodingUTF8));
-    return escaped;
+NSString *KRSHTTPValueEncode(NSString *value) {
+  // Encode all the reserved characters, per RFC 3986
+  // (<http://www.ietf.org/rfc/rfc3986.txt>)
+  NSString *escaped =
+      (NSString *)CFBridgingRelease(CFURLCreateStringByAddingPercentEscapes(
+          kCFAllocatorDefault, (CFStringRef)value, NULL,
+          (CFStringRef) @"!*'();:@&=+$,/?%#[]", kCFStringEncodingUTF8));
+  return escaped;
 }
 
-NSString * KRSHTTPValue(NSString *keyPath, id value) {
+NSString *KRSHTTPValue(NSString *keyPath, id value) {
 
-    NSString *output = nil;
-        
-    if ( [value isKindOfClass:[NSString class]] )
-    {
-        output = [KRSQueryDataSerializer stringFromUnescapedString:value withKeyPath:keyPath];
-    }
-    else if ( [value isKindOfClass:[NSNumber class]] )
-    {
-        output = [KRSQueryDataSerializer stringFromUnescapedString:[value stringValue] withKeyPath:keyPath];
-    }
-    else if ( [value isKindOfClass:[NSArray class]] )
-    {
-        output = [KRSQueryDataSerializer stringFromArray:value withKeyPath:keyPath];
-    }
-    else if ( [value isKindOfClass:[NSDictionary class]] )
-    {
-        output = [KRSQueryDataSerializer stringFromDictionary:value withKeyPath:keyPath];
-    }
+  NSString *output = nil;
 
-    return output;
+  if ([value isKindOfClass:[NSString class]]) {
+    output = [KRSQueryDataSerializer stringFromUnescapedString:value
+                                                   withKeyPath:keyPath];
+  } else if ([value isKindOfClass:[NSNumber class]]) {
+    output =
+        [KRSQueryDataSerializer stringFromUnescapedString:[value stringValue]
+                                              withKeyPath:keyPath];
+  } else if ([value isKindOfClass:[NSArray class]]) {
+    output = [KRSQueryDataSerializer stringFromArray:value withKeyPath:keyPath];
+  } else if ([value isKindOfClass:[NSDictionary class]]) {
+    output =
+        [KRSQueryDataSerializer stringFromDictionary:value withKeyPath:keyPath];
+  }
+
+  return output;
 }
 
 @implementation KRSQueryDataSerializer
 
++ (NSString *)stringFromDictionary:(NSDictionary *)dictionary {
+  return [self stringFromDictionary:dictionary withKeyPath:nil];
+}
+
 + (NSString *)stringFromDictionary:(NSDictionary *)dictionary
-{
-    return [self stringFromDictionary:dictionary withKeyPath:nil];
-}
+                       withKeyPath:(NSString *)keyPath {
+  NSMutableArray *pairs = [NSMutableArray array];
 
-+ (NSString *)stringFromDictionary:(NSDictionary *)dictionary withKeyPath:(NSString *)keyPath
-{
-    NSMutableArray *pairs = [NSMutableArray array];
-    
-    for (NSString *key in [dictionary keyEnumerator])
-    {
-        id value = [dictionary objectForKey:key];
-        NSString *output = KRSHTTPValue(KRSHTTPBuildKeyPath(keyPath, key), value);
-        
-        if ( output )
-        {
-            [pairs addObject:output];
-        }
+  for (NSString *key in [dictionary keyEnumerator]) {
+    id value = [dictionary objectForKey:key];
+    NSString *output = KRSHTTPValue(KRSHTTPBuildKeyPath(keyPath, key), value);
+
+    if (output) {
+      [pairs addObject:output];
     }
-    
-    return [pairs componentsJoinedByString:@"&"];
+  }
+
+  return [pairs componentsJoinedByString:@"&"];
 }
 
-+ (NSString *)stringFromArray:(NSArray *)array withKeyPath:(NSString *)keyPath
-{
-    NSMutableArray *pairs = [NSMutableArray array];
++ (NSString *)stringFromArray:(NSArray *)array withKeyPath:(NSString *)keyPath {
+  NSMutableArray *pairs = [NSMutableArray array];
 
-    NSInteger index = 0;
+  NSInteger index = 0;
 
-    for ( id value in array )
-    {
-        NSString *key = [NSString stringWithFormat:@"%d", index];
-        NSString *output = KRSHTTPValue(KRSHTTPBuildKeyPath(keyPath, key), value);
-        
-        if ( output )
-        {
-            [pairs addObject:output];
-        }
-    
-        index++;
+  for (id value in array) {
+    NSString *key = [NSString stringWithFormat:@"%ld", (long)index];
+    NSString *output = KRSHTTPValue(KRSHTTPBuildKeyPath(keyPath, key), value);
+
+    if (output) {
+      [pairs addObject:output];
     }
 
-    return [pairs componentsJoinedByString:@"&"];
+    index++;
+  }
+
+  return [pairs componentsJoinedByString:@"&"];
 }
 
-+ (NSString *)stringFromUnescapedString:(NSString *)string withKeyPath:(NSString *)keyPath
-{
-    NSString *escapedString = KRSHTTPValueEncode(string);
-    
-    return [NSString stringWithFormat:@"%@=%@", keyPath, escapedString];
++ (NSString *)stringFromUnescapedString:(NSString *)string
+                            withKeyPath:(NSString *)keyPath {
+  NSString *escapedString = KRSHTTPValueEncode(string);
+
+  return [NSString stringWithFormat:@"%@=%@", keyPath, escapedString];
 }
 
 @end
